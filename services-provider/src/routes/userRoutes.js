8e67c68df1a792a1889d79926711ff32364922f2
waqas0929 <waqasraza0929@gmail.com>
2024-07-05 02:22:44 +0500

    create a roleMiddleware

--- services-provider/src/routes/userRoutes.js
+++ services-provider/src/routes/userRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/userController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import roleMiddleware from "../middlewares/roleMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
+
+userRouter.get;
+"/admin",
+  authMiddleware,
+  roleMiddleware("admin"),
+  (req, res) => {
+    res.send("Admin content");
+  };
 
 export default userRouter;
