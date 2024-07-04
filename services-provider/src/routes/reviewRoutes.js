b0729fa1d67e15acf394f7256c2cd0f9151caf7b
Waqas Raza <128093321+waqas0929@users.noreply.github.com>
2024-07-05 00:44:30 +0500

    Delete services-provider/src directory

--- services-provider/src/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createReview, getReviews } from "../controllers/reviewController.js";
-
-const reviewRouter = express.Router();
-
-reviewRouter.post("/create", createReview);
-reviewRouter.get("/", getReviews);
-
-export default reviewRouter;
