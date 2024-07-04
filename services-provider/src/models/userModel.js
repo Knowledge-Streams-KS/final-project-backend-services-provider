9f834ca867fa76cf5a12338151385a6a7ca203e3
waqas0929 <waqasraza0929@gmail.com>
2024-07-04 17:21:33 +0500

    create a user model

---
+++ services-provider/src/models/userModel.js
@@ -0,0 +1,37 @@
+import sequelize from "../db/config.js";
+
+const userModel = sequelize.define("User", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUID4,
+    primaryKey: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: null,
+    defaultValue: " user ",
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+  },
+  address: {
+    type: DataTypes.STRING,
+  },
+});
+
+export default userModel;
