c9bbcb4d5f28f1775bcbdd40e454787f89b5fe03
waqas0929 <waqasraza0929@gmail.com>
2024-07-05 00:42:07 +0500

    middleware created

--- services-provider/src/routes/bookingRoutes.js
+++ services-provider/src/routes/bookingRoutes.js
@@ -3,10 +3,11 @@ import {
   createBooking,
   getBookings,
 } from "../controllers/bookingController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const bookingRouter = express.Router();
 
-bookingRouter.post("/create", createBooking);
-bookingRouter.get("/", getBookings);
+bookingRouter.post("/create", authMiddleware, createBooking);
+bookingRouter.get("/", authMiddleware, getBookings);
 
 export default bookingRouter;
